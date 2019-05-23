9ede173c728fa1eea05d20cd02696027d871243f
Chase <chasewilliams98@gmail.com>
2019-05-23 09:22:46 -0400

    Enhanced Product Functions

--- src/main/java/com/prs/web/ProductController.java
+++ src/main/java/com/prs/web/ProductController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -55,9 +56,9 @@ public class ProductController {
 	}
 	
 	@PostMapping("/fromFile")
-	public List<JsonResponse> addFromFile(@RequestBody Vendor vendor) {
+	public List<JsonResponse> addFromFile(@RequestBody Vendor vendor, @RequestParam String path) {
 		List<JsonResponse> jr = new ArrayList<JsonResponse>();
-		ProductTextFile ptf = new ProductTextFile();
+		ProductTextFile ptf = new ProductTextFile(path);
 		try {
 			List<Product> products = ptf.getAll(vendor);
 			for (Product p: products) {
